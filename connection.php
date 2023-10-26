eb608112f190e2f316c88b0d5839d8a6d17f5328
Saulo <78312710+Saulo217@users.noreply.github.com>
2023-10-25 22:15:28 -0300

    fix: connection string on pdo constructor

--- connection.php
+++ connection.php
@@ -1,21 +1,18 @@
 <?php
 
-function NewConnection($banco=null){
-    $server="127.0.0.1:3306";
-    $user="root";
-    $password="";
+function NewConnection($banco = null)
+{
+    $server = "127.0.0.1:3306";
+    $user = "root";
+    $password = "";
 
+    try {
+        $pdo = new PDO("mysql:host=$server,dbname=" . $banco, "$user", "$password");
 
+    } catch (PDOException $erro) {
+        echo "Erro: " . $erro->getMessage();
 
-    try{
-    $pdo= new PDO("mysql:host=$server,dbname=$user,$password");
-
-
-    }catch(PDOException $erro){
-    echo "Erro: ".$erro->getMessage();
-    
-}
+    }
     return $pdo;
 
 }
-?>
