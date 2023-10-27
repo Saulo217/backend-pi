52d0fb0058ce6814bc486e248508bef754f70b78
Murilo <127051508+murilo587@users.noreply.github.com>
2023-10-26 21:15:29 -0300

    Murilo: Classe Usuario

---
+++ model/usuario.php
@@ -0,0 +1,137 @@
+<?php
+
+class Usuario
+{
+    private String $email;
+    private String $nome;
+    private  $data_nascimento;
+    private  $foto;
+    private String $senha;
+    private String $usuario;
+    private String $cargo;
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        return $this->email = $email;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        return $this->nome = $nome;
+    }
+
+    public function getData_nascimento()
+    {
+        return $this->data_nascimento;
+    }
+
+    public function setData_nascimento($data_nascimento)
+    {
+        return $this->data_nascimento = $data_nascimento;
+    }
+
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    public function setFoto($foto)
+    {
+        return $this->foto = $foto;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    public function setSenha($senha)
+    {
+        return $this->senha = $senha;
+    }
+
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    public function setUsuario($usuario)
+    {
+        return $this->usuario = $usuario;
+    }
+
+    public function getCargo()
+    {
+        return $this->cargo;
+    }
+
+    public function setCargo($cargo)
+    {
+        return $this->cargo = $cargo;
+    }
+
+    public function create(PDO $pdo)
+    {
+        $sql = "INSERT INTO Usuario(email, nome, data_nascimento, foto, senha, usuario, cargo) VALUES
+        (:email, :nome, :data_nascimento, :foto, :senha, :usuario, :cargo)";
+
+        try {
+            $sth = $pdo->prepare($sql);
+            $sth->bindValue(":email", $this->getEmail());
+            $sth->bindValue(":nome", $this->getNome());
+            $sth->bindValue(":data_nascimento", $this->getData_nascimento());
+            $sth->bindValue(":foto", $this->getFoto());
+            $sth->bindValue(":senha", $this->getSenha());
+            $sth->bindValue(":usuario", $this->getUsuario());
+            $sth->bindValue(":cargo", $this->getCargo());
+            $sth->execute();
+
+            $this->read($pdo);
+
+            echo "Usuario Cadastrado <br>";
+        } catch (PDOException $error) {
+            echo "Error: " . $error->getMessage();
+        }
+    }
+
+    public function read(PDO $pdo)
+    {
+        $sql = "SELECT * FROM Usuario";
+
+        try {
+            $sth = $pdo->prepare($sql);
+            $sth->execute();
+
+            $array = $sth->fetchAll(PDO::FETCH_ASSOC);
+            echo "Listado com successo <br>";
+
+            return $array;
+        } catch (PDOException $error) {
+            echo "Error: " . $error->getMessage();
+        }
+    }
+
+    public function delete(PDO $pdo)
+    {
+        $sql = "DELETE FROM Usuario WHERE email = :email";
+        try {
+            $sth = $pdo->prepare($sql);
+            $sth->bindValue(":email", $this->getEmail());
+            $sth->execute();
+            echo "Deletado com successo <br>";
+        } catch (PDOException $error) {
+            echo "Error: " . $error->getMessage();
+        }
+    }
+}
+?>
