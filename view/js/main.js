21684a701cd581103e8b6b93f0ff5774e5b4f1ff
Murilo <127051508+murilo587@users.noreply.github.com>
2023-10-30 20:21:22 -0300

    modifications on main.js

--- view/js/main.js
+++ view/js/main.js
@@ -1,9 +1,13 @@
 function goToPage(pageName) {
-  if (pageName === 'index') {
-    console.log('teste');
-    return (window.location.href = pageName + '.php');
+  if (pageName === "index") {
+    console.log("teste")
+    return (window.location.href = pageName + ".html");
   }
 
-  window.location.href =
-    'http://localhost/backend-pi/view/pages/' + pageName + '.php';
+  window.location.href = "http://localhost/backend-pi/view/PWA_PI/pages/" + pageName + ".html";
+}
+
+function goToControllerUsuario(pageName) {
+
+  window.location.href = "http://localhost/backend-pi/controller/usuario/" + pageName + ".php";
 }
