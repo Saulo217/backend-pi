a7a0373de07ad78fc341daeaf45294e5531b6f06
saulo <78312710+Saulo217@users.noreply.github.com>
2023-11-07 14:07:26 -0300

    fix: create database and tables automatically

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-header("Location: ../backend-pi/view/pages/index.php");
+require_once "./connection.php";
+
+$file = "./sql/PI_SQL.sql";
+$sqlFile = fopen($file, "r") or die("Unable to open a file");
+
+$pdo = NewConnection();
+try {
+  $pdo->query(fread($sqlFile, filesize($file)));
+} catch(PDOException $error) {
+  echo "Error: " . $error->getMessage();
+}
+
+fclose($sqlFile);
+
+header("location: ../backend-pi/view/pages/index.php");
