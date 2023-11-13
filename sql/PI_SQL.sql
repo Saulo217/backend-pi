9908ab2280c04479a1a955c435370986973d42f4
Saulo <78312710+Saulo217@users.noreply.github.com>
2023-11-13 17:15:19 -0300

    feat: create inserts

--- sql/PI_SQL.sql
+++ sql/PI_SQL.sql
@@ -122,3 +122,35 @@ CREATE TABLE
         CONSTRAINT fk_historico_umidade FOREIGN KEY (id_umidade) REFERENCES umidade (id_umidade),
         CONSTRAINT fk_historico_minhas_plantas FOREIGN KEY (id_planta) REFERENCES minhas_plantas (id_planta)
     );
+
+
+NSERT INTO Usuario VALUES ("okamoto@gmail.com", "Okamoto", "2023-07-07", "usuario.png", "12345", "okamoto", "ROLE_USUARIO"),
+("odake@gmail.com", "Odake", "2023-08-08", "usuario.png", "12345", "odake", "ROLE_USUARIO"), ("hioji@gmail.com", "Hioji", "2023-09-09", "usuario.png", "12345", "hioji", "ROLE_USUARIO");
+ 
+INSERT INTO bugs(descricao, data_contato, data_correcao) VALUES ("Não consigo excluir minha planta!", "2023-01-01", "2023-02-02"), ("Carregamento lento da enciclopédia", "2023-05-05", "2023-06-07"), ("Erro nos sensores", "2023-08-08", "2023-09-12");
+ 
+INSERT INTO interacao(id_bug, email_usuario) VALUES (1, "okamoto@gmail.com"), (2, "odake@gmail.com"), (3, "odake@gmail.com");
+ 
+INSERT INTO irrigacao (quantidade, ligado, tempo) VALUES (10.32, true, "01:30"), (32.10, false, "00:45"), (40.123, false, "01:00");
+ 
+INSERT INTO umidade (ligado, umidade, data_captura) VALUES (true, 30.0, "2023-01-01 06:30"), (true, 31.05, "2023-02-01 09:32"), (true, 32.10, "2023-04-03 10:30");
+ 
+INSERT INTO temperatura (ligado, temperatura, data_captura) VALUES (true, 12.50, "2023-01-01 06:30"), (true, 41.05, "2023-02-01 09:32"), (true, 10.10, "2023-04-03 10:30");
+ 
+INSERT INTO nivel_agua (ligado, nivel_agua, data_captura) VALUES (true, 89.0, "2023-01-01 06:30"), (true, 32.05, "2023-02-01 09:32"), (true, 90.10, "2023-04-03 10:30");
+ 
+INSERT INTO iluminacao (ligado, iluminacao, data_captura) VALUES (true, 21.10, "2023-01-01 06:30"), (true, 19.05, "2023-02-01 09:32"), (true, 30.10, "2023-04-03 10:30");
+INSERT INTO plantas_ornamentais VALUES ("Phalaenopsis spp", "Orquídea borboleta", "2023-10-10", "2023-11-14", 5, 30, 25, 15, 23, "orquidea_borboleta.png"), ("Cattleya spp", "Orquídea rainha", "2024-01-10", "2023-03-30", 3, 35, 35, 15, 60, "orquidea_rainha.png"), ("Dendrobium spp", "Orquídea bambu", "2023-12-01", "2024-01-30", 6, 36.2, 56.32, 12.51, 74.23, "orquidea_bambu.png");
+ 
+INSERT INTO dicas (titulo, subtitulo, corpo, imagens_icons, nome_cientifico) VALUES ("Cuidados com a Orquídea Borboleta", "Temperatura", "Cuidado com a temperatura dessa planta, pois ela é muito sensível", "calor.png", "Phalaenopsis spp"), ("Adubagem com a orquídea bambu", "Como realizar a adubação de forma correta", "Para realizar a adubação dessa planta, é necessário a utilização de cascas de ovos e muitos restos de frutas", "adubagem.png", "Dendrobium spp"), 
+("Irrigação Excessiva em Orquídeas Rainhas", "Os perígos de utilizar muita água", "As orquídeas rainhas possuem uma alta vulnerabilidade a grandes quantidades de água, podendo ocasionar em problemas de saúde na planta", "agua.png", "Cattleya spp");
+ 
+INSERT INTO minhas_plantas(data_nascimento, apelido, cor, email_usuario, nome_cientifico) VALUES 
+("2023-05-01", "Vermelinha", "Vermelha-Rosa", "okamoto@gmail.com", "Cattleya spp"),
+("2023-10-10", "Azulzinha", "Azul-Marinho", "hioji@gmail.com", "Phalaenopsis spp"),
+("2023-11-10", "Amarelinha", "Amarelo", "odake@gmail.com", "Dendrobium spp");
+ 
+INSERT INTO historico (id_iluminacao, id_nivel_agua, id_temperatura, id_umidade, id_planta) VALUES 
+(1, 1, 1, 1, 1),
+(2, 2, 2, 2, 2),
+(3, 3, 3, 3, 3);
